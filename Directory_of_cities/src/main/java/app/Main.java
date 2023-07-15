4e7e90c1f085259c6a196b38b2546e5cc7ac6aaf
DenisGIT1 <111299612+DenisGIT1@users.noreply.github.com>
2023-07-15 20:12:29 +0300

    Add files via upload

---
+++ Directory_of_cities/src/main/java/app/Main.java
@@ -0,0 +1,11 @@
+package app;
+
+import app.fileSource.DirectoryOfCities;
+
+public class Main {
+    public static void main(String[] args) {
+
+        DirectoryOfCities.sberCitiesPrint();
+
+    }
+}
---
+++ Directory_of_cities/src/main/java/app/fileSource/DirectoryOfCities.java
@@ -0,0 +1,60 @@
+package app.fileSource;
+
+import app.entity.Cities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DirectoryOfCities {
+
+    public List<Cities> objectConverter(List<String[]> arrays) {
+        List<Cities> citiesList = new ArrayList<>();
+        List<String[]> abc = arrays;
+        for (int i = 0; i < arrays.size(); i++) {
+            String[] str = arrays.get(i);
+            for (int j = 0; j < str.length; j++) {
+                if (str.length == 6) {
+                    String name = str[1];
+                    String region = str[2];
+                    String district = str[3];
+                    String population = str[4];
+                    String foundation = str[5];
+                    Cities cities = new Cities(name, region, district, population, foundation);
+                    citiesList.add(cities);
+                    break;
+                } else if (str.length == 5) {
+                    String name = str[1];
+                    String region = str[2];
+                    String district = str[3];
+                    String population = str[4];
+                    String foundation = null;
+                    Cities cities = new Cities(name, region, district, population, foundation);
+                    citiesList.add(cities);
+                    break;
+                }
+            }
+        }
+        return citiesList;
+    }
+
+    public void printCities(List<Cities> list) {
+        List<Cities> xx = list;
+        for (int i = 0; i < list.size(); i++) {
+            Cities obj = list.get(i);
+            System.out.println(obj);
+            System.out.println();
+        }
+    }
+
+    public static void sberCitiesPrint() {
+        JavaFileScanner object = new JavaFileScanner();
+        Integer length = object.rowCounter();
+        String[] arr = object.stringArrayConverter(length);
+        List<String[]> listarr = object.converterToListOfArraysOfStrings(arr, length);
+
+        DirectoryOfCities directory = new DirectoryOfCities();
+        List<Cities> city = directory.objectConverter(listarr);
+        System.out.println();
+        directory.printCities(city);
+    }
+}
